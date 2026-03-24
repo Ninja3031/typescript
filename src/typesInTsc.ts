60f5372d9584583cacf535488d23186a8b1c12a1
Ninja3031 <chauhanaadirajsingh@gmail.com>
2026-03-24 23:59:10 +0530

    typeof study

---
+++ src/typesInTsc.ts
@@ -0,0 +1,11 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "A7RZ"
+
+let chaiFlavour:string = 'masala tea'
+
+chaiFlavour = 'Ginger Tea'
+
+let chaiOrder:boolean
