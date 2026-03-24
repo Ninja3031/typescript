ea4d90ae2934953ff8c6714905eedab6b59f6f32
Ninja3031 <chauhanaadirajsingh@gmail.com>
2026-03-24 15:41:31 +0530

    setting up typescript

--- basic.ts
+++
@@ -1,7 +0,0 @@
-function greet(name: string): string {
-    return `Hello ${name}`;
-}
-
-console.log(greet("Aadiraj"));
-console.log(greet(42));
-
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function greet(person : string) : string {
+    return `Hello ${person} welcome to new tutorial of javascript`;
+}
+
+const username: string = "Chai aur Typescript";
+
+console.log(greet(username));
